b679d06fd4d612194955133271892dd3718bdd40
Maoxim <76590045+Maoxim@users.noreply.github.com>
2021-09-14 17:01:31 +0800

    Add files via upload

---
+++ mybatis-project/mybatis-01/src/main/java/com/maoxin/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.maoxin.dao;
+
+import com.maoxin.pojo.User;
+
+import java.util.List;
+
+public interface UserDao {
+    public List<User> getUserList();
+}
+
---
+++ mybatis-project/mybatis-01/src/test/java/com/maoxin/dao/UserDaoTest.java
@@ -0,0 +1,29 @@
+package com.maoxin.dao;
+
+import com.maoxin.pojo.User;
+import com.maoxin.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserDaoTest {
+    @Test
+    public void test(){
+
+        //1.获取SqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        //2.执行SQL
+        // 方式一：getMapper
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        List<User> userList = userDao.getUserList();
+        for (User user : userList) {
+            System.out.println(user.getPwd());
+        }
+
+        //关闭sqlSession
+        sqlSession.close();
+    }
+
+
+}
